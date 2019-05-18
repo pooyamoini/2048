f7fe830efdf58a56ec5543cb844e02d892ca3a57
pooya <pooyamoini2000@gmail.com>
2019-05-18 15:39:21 +0430

    key press is going to be implemented

--- src/sample/PlayDisplay.java
+++ src/sample/PlayDisplay.java
@@ -1,54 +1,54 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.awt.*;
-
 public class PlayDisplay {
-    static Group root = new Group();
+    private static Group root = new Group();
     static Stage playDisplay = new Stage();
     private static Scene scenePlay;
     private static final float RECTANGLE_HEIGHT = 400 / 4.5f;
     private static final float RECTANGLE_WEIGHT = 400 / 4.5f;
-    private static final Color COLOR_2 = new Color(238 / 255f, 228 / 255f, 218 / 255f, 1);
-    private static final Color COLOR_4 = new Color(237 / 255f, 224 / 255f, 200 / 255f, 1);
-    private static final Color COLOR_8 = new Color(245 / 255f, 199 / 255f, 99 / 255f, 1);
-    private static final Color COLOR_16 = new Color(246 / 255f, 94 / 255f, 59 / 255f, 1);
-    private static Rectangle[] rectangles = new Rectangle[16];
-    private static Text[] labels = new Text[16];
+    private static Rectangle[][] rectangles = new Rectangle[4][4];
+    private static Text[][] labels = new Text[4][4];
 
     static {
         for (int i = 0; i < rectangles.length; i++) {
-            rectangles[i] = new Rectangle(RECTANGLE_WEIGHT, RECTANGLE_HEIGHT);
-            rectangles[i].setArcHeight(35);
-            rectangles[i].setArcWidth(35);
-            rectangles[i].setFill(COLOR_2);
-            if (i % 4 == 0) {
-                rectangles[i].setX(50);
-                rectangles[i].setY((i / 4f) * RECTANGLE_WEIGHT + 50);
-                if (i != 0)
-                    rectangles[i].setY(rectangles[i - 4].getY() + RECTANGLE_WEIGHT + 10);
-            } else {
-                rectangles[i].setX(rectangles[i - 1].getX() + RECTANGLE_WEIGHT + 15);
-                rectangles[i].setY(rectangles[i - 1].getY());
+            for (int j = 0; j < rectangles[i].length; j++) {
+                rectangles[i][j] = new Rectangle(RECTANGLE_WEIGHT, RECTANGLE_HEIGHT);
+                rectangles[i][j].setArcHeight(35);
+                rectangles[i][j].setArcWidth(35);
+                rectangles[i][j].setFill(COLORS.getColor0());
+                if (j == 0) {
+                    rectangles[i][j].setX(50);
+                    rectangles[i][j].setY((i / 4f) * RECTANGLE_WEIGHT + 50);
+                    if (i != 0)
+                        rectangles[i][j].setY(rectangles[i - 1][j].getY() + RECTANGLE_WEIGHT + 10);
+                } else {
+                    rectangles[i][j].setX(rectangles[i][j - 1].getX() + RECTANGLE_WEIGHT + 15);
+                    rectangles[i][j].setY(rectangles[i][j - 1].getY());
+                }
+                root.getChildren().add(rectangles[i][j]);
             }
-            root.getChildren().add(rectangles[i]);
         }
     }
 
     static {
         for (int i = 0; i < labels.length; i++) {
-            labels[i] = new Text(Integer.toString(i));
-            labels[i].setX(rectangles[i].getX() + RECTANGLE_WEIGHT / 2 - 3);
-            labels[i].setY(rectangles[i].getY() + RECTANGLE_HEIGHT / 2 + 5);
-            labels[i].setScaleX(1.8);
-            labels[i].setScaleY(1.8);
-            root.getChildren().add(labels[i]);
+            for (int j = 0; j < labels[i].length; j++) {
+                labels[i][j] = new Text();
+                labels[i][j].setX(rectangles[i][j].getX() + RECTANGLE_WEIGHT / 2 - 3);
+                labels[i][j].setY(rectangles[i][j].getY() + RECTANGLE_HEIGHT / 2 + 5);
+                labels[i][j].setScaleX(2.3);
+                labels[i][j].setScaleY(2.3);
+                root.getChildren().add(labels[i][j]);
+            }
         }
     }
 
@@ -59,9 +59,29 @@ public class PlayDisplay {
         playDisplay.setScene(scenePlay);
 
         Main.window.setOnCloseRequest(e -> {
-            Game.setCurrentGame(null);
+            Game.setCurrentGameNull();
             Main.window.setScene(Main.scene);
         });
+
+        scenePlay.setOnKeyPressed(event -> {
+            switch (event.getCode()) {
+                case LEFT:
+                    Game.getCurrentGame().left();
+                    break;
+                case UP:
+                    Main.window.close();
+                    break;
+                case RIGHT:
+                    Main.window.close();
+                    break;
+                case DOWN:
+                    Main.window.close();
+                    break;
+            }
+        });
+
+        scenePlay.setOnKeyReleased(event -> showNumbers());
+
         showNumbers();
 
         playDisplay.show();
@@ -75,9 +95,50 @@ public class PlayDisplay {
     private static void showNumbers() {
         for (int i = 0; i < Game.getCurrentGame().getBoard().length; i++) {
             for (int j = 0; j < Game.getCurrentGame().getBoard()[i].length; j++) {
-                if (Game.getCurrentGame().getBoard()[i][j].getValue() == 0) {
-                    root.getChildren().remove(labels[4 * i + j]);
-                } else labels[4 * i + j].setText(Integer.toString(Game.getCurrentGame().getBoard()[i][j].getValue()));
+                root.getChildren().remove(labels[i][j]);
+                if (!Game.getCurrentGame().getBoard()[i][j].isEmpty()) {
+                    labels[i][j].setText(Integer.toString(Game.getCurrentGame().getBoard()[i][j].getValue()));
+                    root.getChildren().add(labels[i][j]);
+                    switch (Game.getCurrentGame().getBoard()[i][j].getValue()) {
+                        case 2:
+                            rectangles[i][j].setFill(COLORS.getColor2());
+                            break;
+                        case 4:
+                            rectangles[i][j].setFill(COLORS.getColor4());
+                            break;
+                        case 8:
+                            rectangles[i][j].setFill(COLORS.getColor8());
+                            break;
+                        case 16:
+                            rectangles[i][j].setFill(COLORS.getColor16());
+                            break;
+                        case 32:
+                            rectangles[i][j].setFill(COLORS.getColor32());
+                            break;
+                        case 64:
+                            rectangles[i][j].setFill(COLORS.getColor64());
+                            break;
+                        case 128:
+                            rectangles[i][j].setFill(COLORS.getColor128());
+                            break;
+                        case 256:
+                            rectangles[i][j].setFill(COLORS.getColor256());
+                            break;
+                        case 512:
+                            rectangles[i][j].setFill(COLORS.getColor512());
+                            break;
+                    }
+                }
+            }
+        }
+        initializeEmpty();
+    }
+
+    private static void initializeEmpty(){
+        for (int i = 0; i < Game.getCurrentGame().getBoard().length; i++) {
+            for (int j = 0; j < Game.getCurrentGame().getBoard()[i].length; j++) {
+                if (Game.getCurrentGame().getBoard()[i][j].isEmpty())
+                    rectangles[i][j].setFill(COLORS.getColor0());
             }
         }
     }
